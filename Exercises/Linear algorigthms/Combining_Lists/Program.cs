dc0e3ee04bc09cb1cb5906a5377e8c798af62059
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-02 17:03:29 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Combining_Lists/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combining_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> combinedList = firstList;
+            combinedList.AddRange(secondList);
+            combinedList.Sort();
+            Console.WriteLine(String.Join(" ", combinedList));
+        }
+    }
+}
