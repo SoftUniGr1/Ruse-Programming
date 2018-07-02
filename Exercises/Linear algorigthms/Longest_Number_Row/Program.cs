2d380eba0989e3fb7e9e0a512ac4c3c0b3bc995b
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-02 17:06:22 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Longest_Number_Row/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Number_Row
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            if (numbers.Count == 0)
+            {
+                throw new InvalidOperationException("List is empty");
+            }
+            int maxAmount = 0;
+            int currentAmount = 0;
+            int maxNumber = numbers[0];
+            
+            for (int i = 0; i<numbers.Count; i++)
+            {
+                currentAmount++;
+                int currentNumber = numbers[i];
+                if (currentNumber == numbers[i+1])
+                {
+                    
+                    if (currentAmount > maxAmount)
+                    {
+                        maxAmount = currentAmount;
+                        maxNumber = currentNumber;
+                    }
+                }
+                else
+                {
+                    currentAmount = 0;
+                }
+            }
+            for (int i = 0; i < maxAmount; i++)
+            {
+                Console.Write(maxAmount + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
