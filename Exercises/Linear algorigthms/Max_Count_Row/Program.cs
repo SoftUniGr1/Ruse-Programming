d1068909dcfaad34b94c617cc83010a8da90e099
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-02 17:13:31 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Max_Count_Row/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_Count_Row
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ', ',').Select(int.Parse).ToArray();
+            int currVal = nums[0];
+            int maxVal = currVal;
+            int currLength = 1;
+            int maxLength = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (currVal == nums[i])
+                {
+                    currLength++;
+                }
+                else
+                {
+                    if (currLength > maxLength)
+                    {
+                        maxLength = currLength;
+                        maxVal = currVal;
+                    }
+                    
+                        currLength = 1;
+                        currVal = nums[i];
+
+                    
+                }
+                
+            }
+
+            Console.WriteLine("{0} - {1} times", maxVal, maxLength);
+        }
+    }
+}
