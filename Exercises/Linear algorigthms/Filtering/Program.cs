4fdac962ae837b87611837585c8d42f30e22a19c
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-02 17:04:56 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Filtering/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filtering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine().Split(' ', ',').Select(double.Parse).ToArray();
+            List<double> list = new List<double>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    list.Add(arr[i]);
+                }
+            }
+            arr = list.ToArray();
+            Console.Write("{");
+            Console.Write(String.Join(", ", arr));
+            Console.WriteLine("}");
+        }
+    }
+}
