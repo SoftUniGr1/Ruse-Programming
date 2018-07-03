fe49719ae3fd665eac90666888bc6162a5e3cdda
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-03 15:43:35 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Reverse_Sort_Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Sort_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Stack<int> stack = new Stack<int>(arr.Length);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                stack.Push(arr[i]);
+            }
+            
+            Console.WriteLine(String.Join(" ", stack));
+        }
+    }
+}
