4ecf4d2ee387883728f64a0df9a71bf2aae8c0e5
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-03 14:50:47 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Sorting_Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            List<string> newWords = new List<string>();
+            newWords.AddRange(words);
+            newWords.Sort();
+            Console.WriteLine(String.Join(" ", newWords));
+        }
+    }
+}
