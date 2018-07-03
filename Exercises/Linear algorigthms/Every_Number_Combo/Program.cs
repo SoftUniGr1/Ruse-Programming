e7695f5404748947e070803e7a23cee5e7f3f93b
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-03 14:31:19 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Every_Number_Combo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urok_3_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            if (nums.Length == 2)
+            {
+                for (int i1 = 1; i1<=nums[0]; i1++)
+                {
+                    for (int i2 = 1; i2 <= nums[1]; i2++)
+                    {
+                        Console.WriteLine("{0} {1}", i2, i1);
+                    }
+                }
+            }
+        }
+    }
+}
