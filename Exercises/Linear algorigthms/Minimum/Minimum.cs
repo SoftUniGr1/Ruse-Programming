341fc2e65db2a1c0b6c98429187e5e1510eb1187
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-03 14:29:28 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Minimum/Minimum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urok_3_Exercise
+{
+    class Minimum
+    {
+        public Minimum(int[] numbers)
+        {
+            int smallestNum = int.MaxValue;
+            if (numbers.Length == 0)
+            {
+                throw new InvalidOperationException("Array is empty");
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (smallestNum > numbers[i])
+                {
+                    smallestNum = numbers[i];
+                }
+            }
+            Console.WriteLine("Smallest number is: {0}", smallestNum);
+        }
+    }
+}
