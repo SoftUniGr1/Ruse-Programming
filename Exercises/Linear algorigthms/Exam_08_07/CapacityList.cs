04637f6fd7eab99aca155f27f8e66cc844ab4ba9
Ivan Dosev <35624740+ivan123dosev@users.noreply.github.com>
2018-07-08 12:41:13 +0300

    Add files via upload

---
+++ Exercises/Linear algorigthms/Exam_08_07/CapacityList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSkeleton
+{
+    class CapacityList
+    {
+        private const int InitialCapacity = 2;
+        private Pair[] items;
+        private int startIndex = 0; 
+        private int nextIndex = 0; 
+        public CapacityList(int capacity = InitialCapacity)
+        {
+            this.items = new Pair[capacity];
+        }
+        public int Count
+        {
+            get;
+            private set;
+        }
+        public Pair SumIntervalPairs()
+        {           
+            Pair sum = new Pair(0, 0);
+            for (int i = startIndex; i < nextIndex; i++)
+            {
+                
+                sum.First += items[i].First;
+                sum.Last += items[i].Last;
+            }           
+            return sum;
+            
+        }
+        public Pair Sum()
+        {
+            Pair sum = new Pair(0, 0);
+            for (int i = 0; i < startIndex; i++)
+            {
+                
+                sum.First += items[i].First;
+                sum.Last += items[i].Last;
+            }
+            return sum;
+            
+        }
+        public void Add(Pair item)
+        {
+            CheckCapacity();
+            items[nextIndex] = item;
+            nextIndex++;           
+                    
+        }
+        private void CheckCapacity()
+        {
+            if (nextIndex == items.Length)
+            {
+                Pair check = SumIntervalPairs();
+                items[startIndex] = check;
+                startIndex++;
+                nextIndex = startIndex;
+            }           
+        }
+        public void PrintCurrentState()
+        {
+            for (int i = 0; i < nextIndex; i++)
+            {
+                Console.WriteLine(string.Join("\n", (object)items));
+            }
+            
+        }
+    }
+}
---
+++ Exercises/Linear algorigthms/Exam_08_07/Pair.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSkeleton
+{
+    class Pair
+    {
+        public int First { get; set; }
+
+        public int Last { get; set; }
+
+
+
+        public Pair(int first, int last)
+
+        {
+            this.First = first;
+            this.Last = last;
+            //TODO: Add code
+
+        }
+
+
+
+        public override string ToString()
+
+        {
+            return this.First + " " + this.Last;
+            //TODO: Add code
+
+        }
+
+
+
+        public int Difference()
+
+        {
+            return Math.Abs(this.First - this.Last);
+            //TODO: Add code
+
+        }
+    }
+}
